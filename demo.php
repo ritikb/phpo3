b2988aa46e61378e8630f5f565b92e80f0e11e18
ritikb <ritikb001@gmail.com>
2019-01-30 15:37:06 +0530

    demo page add

---
+++ demo.php
@@ -0,0 +1,24 @@
+<?php
+
+$a=august;
+$b=march;
+$c=june;
+$d=august;
+
+if ($a==$b) {
+ echo" a and b are same"; 
+            }
+elseif($b==$c)
+{
+	echo" b and c are same"; 
+}
+
+elseif($c==$a)
+{
+	echo" c and a are same"; 
+}
+elseif($a==$d)
+{
+	echo" a and d are same"; 
+}
+?>
