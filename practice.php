ddf5416faccad0965873adb16daca375ad3ae50c
ritikb <ritikb001@gmail.com>
2019-02-01 13:33:10 +0530

    all file

---
+++ practice.php
@@ -0,0 +1,47 @@
+<?php
+/*
+$a=10;
+$b=30;
+
+$c=$a+$b*$a;
+
+echo "sum =".$c;
+ 
+$my_car="audi";
+
+echo "<br>".$my_car ."&nbsp;is riding";
+
+echo "<br>".$c.$my_car."&nbsp;is very fast";
+
+$a=20;
+
+for ($a=1; $a<10; $a++) { 
+	
+	echo "<br>".$a;
+}*/
+
+
+echo "hello world!<br>";
+
+$color="red ";
+
+print "my car is &nbsp".$color."mnvgg".$color;
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/1.php
@@ -0,0 +1,14 @@
+<?php
+
+$username ="admin";
+$password ="12345";
+
+if ($username =='admin' && $password =='1234') 
+{
+	echo 'welcome';
+}
+
+else
+{
+	echo "please check your username and password";
+}
